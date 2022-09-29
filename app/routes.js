1f43ff5dc3301708089c0762dd1a704d7172b952
Ming Chan <mingosoft@gmail.com>
2022-09-29 17:03:03 +0100

    Adding sub pages

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,9 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Routes for specific projects
+require('./routes/examples/routes.js')(router);
+require('./routes/exercises/routes.js')(router);
+
+
 module.exports = router
